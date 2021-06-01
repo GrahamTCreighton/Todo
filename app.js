7978bbdaedc07aff1dfba0c2d3b0df0e986a3c1b
grey <guitarrista.1988@hotmail.com>
2021-06-01 15:02:44 +0200

    updated not finished

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 import api from "./api.js";
 
-console.log(api);
-console.log("you beautiful human being")
+let list = document.createElement("ul")
+document.body.appendChild(list)
+
+let toDoList=api.retrieve()
+Object.keys(toDoList).forEach(id => {
+    let task = document.createElement("li")
+    list.appendChild(task)
+    task.innerHTML = toDoList[id].value
+    
+}) // We want a list element per each task that we have in the object 
+
+/* Quiero que mi estructura de html 
+<ul>
+<li>Walk the dog</li>
+<li>Cut the grass</li>
+</ul>
+*/
--- state.js
+++ state.js
@@ -1,3 +1,8 @@
-let toDoList = {};
+let toDoList = {
+    1:{ value: "Walk the dog",
+       completed: true},
+    2:{ value: "cut the grass",
+    completed: false},
+};
 
 export default toDoList;
