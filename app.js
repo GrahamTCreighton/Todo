35b1d230273f82cd6f7303f15afd6557af0db5ee
grey <guitarrista.1988@hotmail.com>
2021-06-03 11:15:36 +0200

    recent changes needs work

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import api from "./api.js";
 /*Trial for creating the input*/
 let newtask = document.createElement("input")
 document.body.appendChild(newtask)
-/*let createButton = document.createElement('button')
-createbutton.innerHTML = "To Do"
-button.onclick // ---> Here I want to activate the create function
+let createButton = document.createElement('button')
+createButton.innerHTML = "add new task";
+createButton.onclick = function(){api.create()} // ---> Here I want to activate the create function, use the text of the input field line 4 as the first parameter of the create function
                                     //insert create function here
-document.body.appendChild(createButton)*/
+document.body.appendChild(createButton)
 
 let list = document.createElement("ul")
 document.body.appendChild(list)
@@ -21,9 +21,19 @@ Object.keys(toDoList).forEach(id => {
     let box = document.createElement("input")
     box.setAttribute("type","checkbox")
     if (toDoList[id].completed){box.setAttribute("checked",true)}
-    box.onclick // Tom says this needs a function after the click. 
+    box.onclick = function(){api.updateStatus} // Needs Amanda's approval 
     task.appendChild(box)
+
+    let clearTask = document.createElement("button")
+    clearTask.innerHTML = "clear"
+    clearTask.onclick = function(){api.remove}
+    box.appendChild(clearTask)
 })
+
+let deleteButton = document.createElement('button')
+deleteButton.innerHTML = "clear all"
+deleteButton.onclick = function(){api.removeAll}
+document.body.appendChild(deleteButton)
 
 
 
